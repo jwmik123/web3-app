f4af4ecbe0235c9d768deaa278f78ae13f495c51
Joël Mik <joel@megosu.com>
2022-02-10 18:09:24 +0100

    removed footer

--- src/App.js
+++ src/App.js
@@ -100,15 +100,6 @@ const App = () => {
           { currentAccount === "" ? (renderNotConnectedContainer()) :
            (<button onClick={askContractToMintNft} className="cta-button connect-wallet-button">Mint NFT</button>)}
         </div>
-        <div className="footer-container">
-          <img alt="Twitter Logo" className="twitter-logo" src={twitterLogo} />
-          <a
-            className="footer-text"
-            href={TWITTER_LINK}
-            target="_blank"
-            rel="noreferrer"
-          >{`built on @${TWITTER_HANDLE}`}</a>
-        </div>
       </div>
     </div>
   );
